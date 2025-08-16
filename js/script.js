1e1048e8b853f3bbff1791ddb05d6f39566cb0a2
Abdulaziz <abdulazizprogrammer777@gmail.com>
2025-08-16 09:25:28 +0500

    style by js

---
+++ js/script.js
@@ -0,0 +1,133 @@
+const section1El = document.querySelector('.section_1'),
+	imgEl1 = section1El.querySelector('img'),
+	h1El1 = section1El.querySelector('h1'),
+	pEl1 = section1El.querySelector('p'),
+	buttonEl1 = section1El.querySelector('button'),
+	section2El = document.querySelector('.section_2'),
+	imgEl2 = section2El.querySelector('img'),
+	h1El2 = section2El.querySelector('h1'),
+	pEl2 = section2El.querySelector('p'),
+	buttonEl2 = section2El.querySelector('button'),
+	section3El = document.querySelector('.section_3'),
+	imgEl3 = section3El.querySelector('img'),
+	h1El3 = section3El.querySelector('h1'),
+	pEl3 = section3El.querySelector('p'),
+	buttonEl3 = section3El.querySelector('button'),
+	allEl = document.querySelector('.all')
+
+allEl.style.display = "flex"
+section1El.style.backgroundColor = "#E28625"
+section2El.style.backgroundColor = "#006971"
+section3El.style.backgroundColor = "#004140"
+section1El.style.width = "920px"
+section2El.style.width = "920px"
+section3El.style.width = "920px"
+section1El.style.height = "500px"
+section2El.style.height = "500px"
+section3El.style.height = "500px"
+
+imgEl1.style.width = "64px"
+imgEl1.style.height = "40px"
+imgEl1.style.paddingTop = "48px"
+imgEl1.style.paddingLeft = "49px"
+imgEl2.style.paddingTop = "48px"
+imgEl2.style.paddingLeft = "49px"
+imgEl3.style.paddingTop = "48px"
+imgEl3.style.paddingLeft = "49px"
+imgEl2.style.width = "64px"
+imgEl2.style.height = "40px"
+imgEl3.style.width = "64px"
+imgEl3.style.height = "40px"
+
+h1El1.style.paddingTop = "35px"
+h1El1.style.paddingLeft = "48px"
+h1El1.style.color = "rgba(242, 242, 242, 1)"
+h1El1.style.fontFamily = "Big Shoulders Display"
+h1El1.style.fontSize = "40px"
+h1El1.style.fontWeight = "600"
+h1El1.style.textTransform = "uppercase"
+
+h1El2.style.paddingTop = "35px"
+h1El2.style.paddingLeft = "48px"
+h1El2.style.color = "rgba(242, 242, 242, 1)"
+h1El2.style.fontFamily = "Big Shoulders Display"
+h1El2.style.fontSize = "40px"
+h1El2.style.fontWeight = "600"
+h1El2.style.textTransform = "uppercase"
+
+h1El3.style.paddingTop = "35px"
+h1El3.style.paddingLeft = "48px"
+h1El3.style.color = "rgba(242, 242, 242, 1)"
+h1El3.style.fontFamily = "Big Shoulders Display"
+h1El3.style.fontSize = "40px"
+h1El3.style.fontWeight = "600"
+h1El3.style.textTransform = "uppercase"
+
+
+pEl1.style.width = "212px"
+pEl1.style.height = "125px"
+pEl1.style.opacity = "0,75"
+pEl1.style.color = "rgba(255, 255, 255, 1)"
+pEl1.style.fontFamily = "Lexend Deca"
+pEl1.style.fontSize = "15px"
+pEl1.style.fontWeight = "400"
+pEl1.style.lineHeight = "25px"
+pEl1.style.letterSpacing = "0px"
+pEl1.style.paddingLeft = "48px"
+pEl1.style.paddingTop = "25px"
+
+pEl2.style.width = "212px"
+pEl2.style.height = "125px"
+pEl2.style.opacity = "0,75"
+pEl2.style.color = "rgba(255, 255, 255, 1)"
+pEl2.style.fontFamily = "Lexend Deca"
+pEl2.style.fontSize = "15px"
+pEl2.style.fontWeight = "400"
+pEl2.style.lineHeight = "25px"
+pEl2.style.letterSpacing = "0px"
+pEl2.style.paddingLeft = "48px"
+pEl2.style.paddingTop = "25px"
+
+pEl3.style.width = "212px"
+pEl3.style.height = "125px"
+pEl3.style.opacity = "0,75"
+pEl3.style.color = "rgba(255, 255, 255, 1)"
+pEl3.style.fontFamily = "Lexend Deca"
+pEl3.style.fontSize = "15px"
+pEl3.style.fontWeight = "400"
+pEl3.style.lineHeight = "25px"
+pEl3.style.letterSpacing = "0px"
+pEl3.style.paddingLeft = "48px"
+pEl3.style.paddingTop = "25px"
+
+
+
+
+buttonEl1.style.width = "146px"
+buttonEl1.style.height = "48px"
+buttonEl1.style.marginLeft = "48px"
+buttonEl1.style.borderRadius = "20px"
+buttonEl1.style.border = "none"
+buttonEl1.style.color = "rgba(226, 134, 37, 1)"
+buttonEl1.style.color.hover = "rgba(233, 238, 238, 1)"
+
+buttonEl2.style.width = "146px"
+buttonEl2.style.height = "48px"
+buttonEl2.style.marginLeft = "48px"
+buttonEl2.style.borderRadius = "20px"
+buttonEl2.style.border = "none"
+buttonEl2.style.color = "rgba(0, 105, 113, 1)"
+buttonEl2.style.color.hover = "rgba(233, 238, 238, 1)"
+
+buttonEl3.style.width = "146px"
+buttonEl3.style.height = "48px"
+buttonEl3.style.marginLeft = "48px"
+buttonEl3.style.borderRadius = "20px"
+buttonEl3.style.border = "none"
+buttonEl3.style.color = "rgba(0, 65, 64, 1)"
+buttonEl3.style.color.hover = "rgba(233, 238, 238, 1)"
+
+
+
+
+console.dir()
